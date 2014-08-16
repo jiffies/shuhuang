4468c2e30e032c252a8be89ce034218553c78169
jiffies <lcqtdwj@gmail.com>
2014-08-16 19:11:19 +0800

    nice ui

--- books/static/books/app.js
+++ books/static/books/app.js
@@ -4,6 +4,7 @@ $(function(){
 			return {
 				title:"new book",
 				author:"none",
+				isNew:true,
 				introduction:""
 			};
 		}
@@ -28,21 +29,37 @@ $(function(){
 			return this;
 		},
 	});
+	var lastBookView=null;
 	var AppView = Backbone.View.extend({
 		el:$("#bookapp"),
 		clitemplate:_.template($("#ctl-template").html()),
-		//events: {
+		events: {
+			"click #nextbook":"nextBook",
 			//"click #next":"nextBook",
 			//"click #prev":"prevBook",
-		//},
+		},
 		initialize:function(){
 			this.listenTo(Books,'add',this.addOne);
-			this.listenTo(Books,'reset',this.addAll);
+			//this.listenTo(Books,'reset',this.addAll);
 			Books.fetch();
 		},
+		nextBook:function(){
+			var newBook=Books.findWhere({isNew:true});
+			if (!newBook){
+				alert("no book!");
+			}
+			newBook.set("isNew",false);
+			if (lastBookView){
+				lastBookView.remove();
+			}
+			var view = new BookView({model:newBook});
+			this.$("#book li").html(view.render().el);
+			lastBookView=view;
+			//alert("show me next book!"+newBook.get('title'));
+		},
 		addOne:function(book){
-			var view = new BookView({model:book});
-			this.$("#book").append(view.render().el);
+			//var view = new BookView({model:book});
+			//this.$("#book").append(view.render().el);
 		},
 		addAll:function(){
 		Books.each(this.addOne,this);
